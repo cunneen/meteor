04cc0b024d5614f2099fa20905b90204f1c4de5a
Mike Cunneen <mike@appworkshop.net>
2024-11-07 22:34:30 +0800

    backporting linux ARM support to v2.x : finally found workaround for node-gyp crash

--- scripts/generate-dev-bundle.sh
+++ scripts/generate-dev-bundle.sh
@@ -18,8 +18,7 @@ cd "$DIR"
 
 extractNodeFromTarGz() {
     LOCAL_TGZ="${CHECKOUT_DIR}/node_${PLATFORM}_v${NODE_VERSION}.tar.gz"
-    if [ -f "$LOCAL_TGZ" ]
-    then
+    if [ -f "$LOCAL_TGZ" ]; then
         echo "Skipping download and installing Node from $LOCAL_TGZ" >&2
         tar zxf "$LOCAL_TGZ"
         return 0
@@ -67,9 +66,9 @@ downloadReleaseCandidateNode() {
 }
 
 # Try each strategy in the following order:
-extractNodeFromTarGz || downloadNodeFromS3 || \
-  downloadOfficialNode14 || downloadReleaseCandidateNode || \
-  downloadUnofficialNode14ARM
+extractNodeFromTarGz || downloadNodeFromS3 ||
+    downloadOfficialNode14 || downloadReleaseCandidateNode ||
+    downloadUnofficialNode14ARM
 
 if [ $? -ne 0 ]; then
     echo "Failed to download Node" >&2
@@ -86,21 +85,20 @@ if [ $ARCH = "i686" ] && [ $OS = "linux" ]; then
 fi
 
 case $OS in
-    macos) MONGO_BASE_URL="https://fastdl.mongodb.org/osx" ;;
-    linux)
-        [ $ARCH = "i686" -o $ARCH = "aarch64" ] &&
-            MONGO_BASE_URL="https://fastdl.mongodb.org/linux" ||
-            MONGO_BASE_URL="https://github.com/meteor/mongodb-builder/releases/download/v${MONGO_VERSION}"
-        ;;
+macos) MONGO_BASE_URL="https://fastdl.mongodb.org/osx" ;;
+linux)
+    [ $ARCH = "i686" -o $ARCH = "aarch64" ] &&
+        MONGO_BASE_URL="https://fastdl.mongodb.org/linux" ||
+        MONGO_BASE_URL="https://github.com/meteor/mongodb-builder/releases/download/v${MONGO_VERSION}"
+    ;;
 esac
 
-
-if [ $OS = "macos" ] && [ "$(uname -m)" = "arm64" ] ; then
-  MONGO_NAME="mongodb-${OS}-x86_64-${MONGO_VERSION}"
-elif [ $OS = "linux" ] && [ "$ARCH" = "aarch64" ] ; then
-  MONGO_NAME="mongodb-linux-aarch64-ubuntu2004-${MONGO_VERSION}"
+if [ $OS = "macos" ] && [ "$(uname -m)" = "arm64" ]; then
+    MONGO_NAME="mongodb-${OS}-x86_64-${MONGO_VERSION}"
+elif [ $OS = "linux" ] && [ "$ARCH" = "aarch64" ]; then
+    MONGO_NAME="mongodb-linux-aarch64-ubuntu2004-${MONGO_VERSION}"
 else
-  MONGO_NAME="mongodb-${OS}-${ARCH}-${MONGO_VERSION}"
+    MONGO_NAME="mongodb-${OS}-${ARCH}-${MONGO_VERSION}"
 fi
 
 MONGO_TGZ="${MONGO_NAME}.tgz"
@@ -120,7 +118,7 @@ export PATH="$DIR/bin:$PATH"
 cd "$DIR/lib"
 # Overwrite the bundled version with the latest version of npm.
 npm install "npm@$NPM_VERSION"
-npm config set python `which python3`
+npm config set python $(which python3)
 which node
 which npm
 npm version
@@ -146,12 +144,13 @@ ls -al "$INCLUDE_PATH"
 # will get confused by the pre-existing modules.
 mkdir "${DIR}/build/npm-server-install"
 cd "${DIR}/build/npm-server-install"
-node "${CHECKOUT_DIR}/scripts/dev-bundle-server-package.js" > package.json
+node "${CHECKOUT_DIR}/scripts/dev-bundle-server-package.js" >package.json
 # XXX For no apparent reason this npm install will fail with an EISDIR
 # error if we do not help it by creating the .npm/_locks directory.
 mkdir -p "${DIR}/.npm/_locks"
-
-if npm install; then
+INSTALL_RESULT=0;
+npm install || INSTALL_RESULT=$?
+if [ ${INSTALL_RESULT}  -ne 0 ]; then
     # npm install failed
 
     # python >= v3.11 now causes node-gyp to crash when it encounters the source
@@ -159,12 +158,15 @@ if npm install; then
     #   See https://github.com/nodejs/node-gyp/issues/2219
     #
     # We'll try patching the input python source and redo 'npm install'
-
+    echo "================================================="
+    echo "= npm install failed, trying to patch input.py ="
 
     FILE_TO_ATTEMPT_PATCH="${DIR}/lib/node_modules/npm/node_modules/node-gyp/gyp/pylib/gyp/input.py"
-    echo "Trying to patch $FILE_TO_ATTEMPT_PATCH"
+    echo "= Trying to patch $FILE_TO_ATTEMPT_PATCH"
 
     sed -E -i "s/build_file_path, 'rU'/build_file_path, 'r'/g" $FILE_TO_ATTEMPT_PATCH
+    echo " = Patched $FILE_TO_ATTEMPT_PATCH; now re-running npm install = "
+    echo "================================================="
     npm install
 fi
 npm shrinkwrap
@@ -196,7 +198,7 @@ cd node_modules
 # Used to delete bulky subtrees. It's an error (unlike with rm -rf) if they
 # don't exist, because that might mean it moved somewhere else and we should
 # update the delete line.
-delete () {
+delete() {
     if [ ! -e "$1" ]; then
         echo "Missing (moved?): $1"
         exit 1
@@ -206,8 +208,7 @@ delete () {
 
 # Since we install a patched version of pacote in $DIR/lib/node_modules,
 # we need to remove npm's bundled version to make it use the new one.
-if [ -d "pacote" ]
-then
+if [ -d "pacote" ]; then
     delete npm/node_modules/pacote
     mv pacote npm/node_modules/
 fi
@@ -222,17 +223,17 @@ find . -path '*/esprima-fb/test' | xargs rm -rf
 
 # Sanity check to see if we're not breaking anything by replacing npm
 INSTALLED_NPM_VERSION=$(cat "$DIR/lib/node_modules/npm/package.json" |
-xargs -0 node -e "console.log(JSON.parse(process.argv[1]).version)")
+    xargs -0 node -e "console.log(JSON.parse(process.argv[1]).version)")
 if [ "$INSTALLED_NPM_VERSION" != "$NPM_VERSION" ]; then
-  echo "Error: Unexpected NPM version in lib/node_modules: $INSTALLED_NPM_VERSION"
-  echo "Update this check if you know what you're doing."
-  exit 1
+    echo "Error: Unexpected NPM version in lib/node_modules: $INSTALLED_NPM_VERSION"
+    echo "Update this check if you know what you're doing."
+    exit 1
 fi
 
 echo BUNDLING
 
 cd "$DIR"
-echo "${BUNDLE_VERSION}" > .bundle_version.txt
+echo "${BUNDLE_VERSION}" >.bundle_version.txt
 rm -rf build CHANGELOG.md ChangeLog LICENSE README.md .npm
 
 tar czf "${CHECKOUT_DIR}/dev_bundle_${PLATFORM}_${BUNDLE_VERSION}.tar.gz" .
